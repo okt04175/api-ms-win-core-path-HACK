5992448d05baa568e221b44134cea6569ba9a4dd
okt04175 <okt04175@yahoo.com.hk>
2024-12-19 05:33:56 +0800

    Update api-ms-win-core-path-blender.c - #include <string.h>

--- api-ms-win-core-path-blender.c
+++ api-ms-win-core-path-blender.c
@@ -24,7 +24,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  * 
  */
-#include "windows.h"
+#include <string.h>
+
+#include <windows.h>
 
 #include <memoryapi.h>
 #pragma comment(lib, "memoryapi.lib")
